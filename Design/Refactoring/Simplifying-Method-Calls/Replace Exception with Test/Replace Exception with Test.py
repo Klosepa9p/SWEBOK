a48968de3384cccc6fda12f14e0f32b5b05c2a7d
klosepa <yusuf_kndl-2002@hotmail.com>
2025-08-04 04:11:16 -0400

    more

--- Design/Refactoring/Simplifying-Method-Calls/Replace Exception with Test/Replace Exception with Test.py
+++ Design/Refactoring/Simplifying-Method-Calls/Replace Exception with Test/Replace Exception with Test.py
@@ -1,15 +1,18 @@
 
-
 values = [10, 20, 30, 40]
 
+#Before refactoring
 def getValueForPeriod(periodNumber):
     try:
         return values[periodNumber]
     except IndexError:
         return 0
 
+
+#Replace Exception with Test
 def getValueForPeriod(self, periodNumber):
     if periodNumber >= len(self.values):
         return 0
     return self.values[periodNumber]
+
 
